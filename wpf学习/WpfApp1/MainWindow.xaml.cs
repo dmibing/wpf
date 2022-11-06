b3ed279c6154f6ef62f6a97f246e4797cc5de80e
xu <xushilei0720@aliyun.com>
2022-11-06 14:34:04 +0800

    提交

---
+++ wpf学习/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Annotations;
+using System.Windows.Media.Animation;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           DoubleAnimation  animation = new DoubleAnimation();
+            animation.From = btn.Width;   //设置动画初始值
+            animation.To = btn.Width -30; //设置动画结束值
+            animation.Duration = TimeSpan.FromSeconds(2);  //动画持续事件
+
+
+            //当前按钮执行动画 
+            btn.BeginAnimation(Button.WidthProperty,animation);
+        }
+    }
+}
